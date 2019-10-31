630117897ad30020fc80fd1cf60593f337049363
xuedongZ <xuedong6624@gmail.com>
2019-10-31 14:15:48 +0800

    firstPage slider

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -1,33 +1,24 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import Tab from "../../common/component/tab"
 import login from '../../store/action/login';
-
+import "../../common/css/index.css"
+let imgData =[
+  require("../../common/images/tab/img1.png"),
+  require("../../common/images/tab/img2.png"),
+  require("../../common/images/tab/img3.png"),
+  require("../../common/images/tab/img4.png")
+]
 function Index(props) {
   // console.log(props);
   return (<div>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <button
-      onClick={
-        ()=>{
-          props.dispatch(login()) 
-        }
-      }
-    >登录</button>
+    <Tab 
+      data = {imgData}
+      render = {(data)=>{
+        return <img src={data} />
+
+      }}
+    />
   </div>
   );
 }
